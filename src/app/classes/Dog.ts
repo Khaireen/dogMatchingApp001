a341e2f637bbcbad970b8aa5b1329abeb0cf2a44
Khaireen <khaira@wp.pl>
2018-04-19 20:49:58 +0200

    added class dog

---
+++ src/app/classes/Dog.ts
@@ -0,0 +1,17 @@
+import { Url } from "url";
+
+export class Dog {
+    dogName: string;
+    kennelName: string;
+    breed: string;
+    color: string;
+    sex: string;
+    birthDate: Date;
+    healthInfo: string;
+    showTitles: string;
+    training: string;
+    additionalInfo: string;
+    dogWebsite: Url;
+    dogImage: File;
+
+}
